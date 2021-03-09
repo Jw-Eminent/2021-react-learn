132c63ef012961739d2f0823039cadc68e7a2053
Wang Junwei <841933550@qq.com>
2021-03-09 10:00:58 +0800

    feat: 登录表单及登录逻辑

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
-import ProjectList from "views/ProjectList";
+// import ProjectList from "views/ProjectList";
+import Login from "views/Login";
 
 function App() {
   return (
     <div className="App">
-      <ProjectList />
+      <Login />
+      {/* <ProjectList /> */}
     </div>
   );
 }
