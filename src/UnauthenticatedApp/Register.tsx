d68093750a078d6d915dc4faf3cd64b95e862f38
Wang Junwei <841933550@qq.com>
2021-03-22 10:57:32 +0800

    feat: 引入emotion及优化登录注册界面样式

--- src/UnauthenticatedApp/Register.tsx
+++ src/UnauthenticatedApp/Register.tsx
@@ -12,21 +12,19 @@ const Register = () => {
   return (
     <Form onFinish={handleSubmit}>
       <Form.Item
-        label="账号"
         name="username"
         rules={[{ required: true, message: "请输入用户名" }]}
       >
         <Input type="text" placeholder="用户名" />
       </Form.Item>
       <Form.Item
-        label="密码"
         name="password"
         rules={[{ required: true, message: "请输入密码" }]}
       >
         <Input type="password" placeholder="密码" />
       </Form.Item>
       <Form.Item>
-        <Button type="primary" htmlType="submit">
+        <Button type="primary" htmlType="submit" block>
           注册
         </Button>
       </Form.Item>
